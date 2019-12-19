9cda344001fc88fb76d0c68bc9e170b8a12e9948
unknown <chandral.thakor@gmail.com>
2019-12-19 14:42:57 +0530

    Added feature to guess the word meant by the user in case they misspelled it.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data/data.json"))
 
@@ -7,6 +8,8 @@ def translate(dictionary_key):
     dictionary_key_lowercase = dictionary_key.lower()
     if dictionary_key_lowercase in data:
         return data[dictionary_key_lowercase]
+    elif len(get_close_matches(dictionary_key, data.keys())) > 0:
+        return "Did you mean '{}'?".format(get_close_matches(dictionary_key, data.keys())[0])
     else:
         return "The word does not exist, please check your word."
 
